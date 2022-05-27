e848b4046413be1eb3485b82479e61ee0b5c562d
Aldo Ponce <aldoponcedelacruz@gmail.com>
2022-05-27 17:11:04 -0500

    mongo

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,12 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
 const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'kaylie.gutkowski13@ethereal.email',
-        pass: 'Prmc5MWfUGCAXQP429'
-    }
-}
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: process.env.ETHEREAL_USER,
+    pass: process.env.ETHEREAL_PWD,
+  },
+};
 
-module.exports = nodemailer.createTransport(mailConfig)
+module.exports = nodemailer.createTransport(mailConfig);
